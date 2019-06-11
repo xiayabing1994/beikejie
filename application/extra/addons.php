2a58928cdf700da9c7a64f613f59dcf373ef4ae7
xiaoli <xiaoli@qq.com>
2019-06-11 10:07:31 +0800

    0611first

---
+++ application/extra/addons.php
@@ -0,0 +1,22 @@
+<?php
+
+return array (
+  'autoload' => false,
+  'hooks' => 
+  array (
+    'get_cfg' => 
+    array (
+      0 => 'litestore',
+    ),
+  ),
+  'route' => 
+  array (
+    '/example$' => 'example/index/index',
+    '/example/d/[:name]' => 'example/demo/index',
+    '/example/d1/[:name]' => 'example/demo/demo1',
+    '/example/d2/[:name]' => 'example/demo/demo2',
+    '/third$' => 'third/index/index',
+    '/third/connect/[:platform]' => 'third/index/connect',
+    '/third/callback/[:platform]' => 'third/index/callback',
+  ),
+);
---
+++ runtime/cache/4e/819c837d54a6ed09abc77a8560a66f.php
@@ -0,0 +1,4 @@
+<?php
+//000000000000
+ exit();?>
+a:1:{s:7:"get_cfg";a:1:{i:0;s:27:"\addons\litestore\Litestore";}}
